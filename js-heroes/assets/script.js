4d7c80decc9d556b5b9efdfe42891dc019751505
Cyd Stumpel <info@cydstumpel.nl>
2026-04-26 14:13:31 +0200

    live

--- js-heroes/assets/script.js
+++ js-heroes/assets/script.js
@@ -8,10 +8,10 @@ const multiplier = 7
 spans.forEach((span, index) => {
   // --i: calc((var(--sibling-index) - 1) / (var(--sibling-count) - 1));
   const i = index / (spans.length - 1)
-  // console.log(span.innerText, i)
+  console.log(span.innerText, i)
 
   // --sin-calc: calc((sin(var(--start-angle) + var(--angle) * var(--i)) * -1 - .5) * var(--multiplier));
-  const sinCalc = (Math.sin(startAngle + angle * i) * -1 - 0.5) * multiplier
+  // const sinCalc = (Math.sin(startAngle + angle * i) * -1)
   console.log(span.innerText, sinCalc)
   // --cos-calc: calc((cos(var(--start-angle) + var(--angle) * var(--i)) * -1) * var(--multiplier));
   const cosCalc = (Math.cos(startAngle + angle * i) * -1) * multiplier
